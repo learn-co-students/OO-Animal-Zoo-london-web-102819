eb3340fd02389aa2c4fde4ae9dfc785a456d8f9e
github username <github email address>
2019-10-31 17:53:51 +0000

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
 
+    attr_accessor :weight, :zoo
+    attr_reader :species, :nickname
+    @@all = []
+
+    def initialize(nickname, weight, species)
+        @nickname = nickname
+        @weight= weight
+        @species = species
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species_name)
+        @@all.select{|animal| animal.species == species_name}
+    end
 end
+
+
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,38 @@
+require_relative 'Animal.rb'
+
 class Zoo
 
+    attr_reader :location
+    attr_accessor :name
+    @@all = []
+
+    def initialize(location, name)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select{|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map{|animal| animal.species}.uniq
+    end
+
+    def animal_by_species(species)
+        animals.select{|animal| animal.species == species}
+    end
+
+    def animal_nicknames
+        animals.map{|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        Zoo.all.select{|zoo| zoo.location == location}
+    end
 end
