95021990ddd5ce08ce60b9175c46ce8bab7c1f6c
Qing <chingwang.qw@gmail.com>
2019-11-01 12:54:23 +0000

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,32 @@
 class Animal
+  attr_reader :species, :nickname
+  attr_accessor :weight, :zoo
+
+  @@all = []
+  def initialize(species, nickname, weight, zoo)
+    @species = species
+    @nickname = nickname
+    @weight = weight
+    @zoo =  zoo
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_species(species_lookup)
+    Animal.all.select {|a| a.species == species_lookup}
+  end
 
 end
+
+
+#An `animal` should be instantiated with the species (e.g. "Cat", "Dog", "Rat"), a numerical weight and a nickname. 
+#Keep in mind that the animal's species and nickname should not be able to change, but its weight can.
+#`Animal#nickname` should return the nickname of the animal.
+#`Animal#weight` should return the weight of the animal.
+# `Animal#species` should return the species of the animal.
+# `Animal.all` should return an array of all the animal instances.
+# `Animal#zoo` should return the zoo instance that the instance belongs to.
+# `Animal.find_by_species` should take in an animal's species as an argument and return an array of all the animals, which are of that species.
--- run.rb
+++ run.rb
@@ -3,8 +3,20 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+## Testing code for the ZOO ##
 
+z1 = Zoo.new("london zoo", "London")
+z2 = Zoo.new("Shanghai zoo", "Shanghai")
+z3 = Zoo.new("New York zoo", "New York")
+
+
+#(species, nickname, weight, location)
+a1 = Animal.new("cat", "lucy", 3, z1)
+a2 = Animal.new("dog", "sophie", 3, z2)
+a3 = Animal.new("chicken", "jiji", 2, z1)
+a4 = Animal.new("panda", "yuanyuan", 5, z2)
+a5 = Animal.new("dog", "heizi", 6, z3)
+a6 = Animal.new("cat", "meow", 3, z1)
 
 binding.pry
 puts "done"
