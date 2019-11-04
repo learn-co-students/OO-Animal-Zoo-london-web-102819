87419e0a7c92088e3d014c7c6f2018a77fb16d45
github username <harr.hughes@gmail.com>
2019-11-04 13:29:35 +0000

    first attempt

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight
+
+    @@all = []
+
+    def initialize(species, nickname, weight)
+        @species = species
+        @nickname = nickname
+        @weight = weight
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def zoo
+        Zoo.all.select { |zoo| zoo.animals.include?(self) }
+    end
+
+    def self.find_by_species(species)
+        a1 = Zoo.all.map { |zoo| zoo.animals }
+        a2 = a1.flatten
+        a2.select { |animal| animal.species == species }
+    end
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,41 @@
 class Zoo
 
+    attr_reader :name, :location
+    attr_accessor :animals
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @animals = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def intake(array_of_animals)
+        self.animals << array_of_animals
+        self.animals = self.animals.flatten
+    end
+
+
+    def animal_species
+        self.animals.map{ |animal| animal.species }.uniq
+    end
+
+    def find_by_species(species)
+        self.animals.select { |animal| animal.species == species }
+    end
+
+    def animal_nicknames
+        self.animals.map { |animal| animal.nickname }
+    end
+
+    def self.find_by_location(location)
+        self.all.select {|zoo| zoo.location == location }
+    end
+
 end
